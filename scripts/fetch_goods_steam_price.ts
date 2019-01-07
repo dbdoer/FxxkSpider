611b2075c2542a8528ae14265219be278509e428
Sammy Liang <sammyliang97@gmail.com>
2019-01-07 11:09:21 +0800

    fix: fixed fetch steam price

--- scripts/fetch_goods_steam_price.ts
+++ scripts/fetch_goods_steam_price.ts
@@ -4,11 +4,11 @@ import { getSteamPrice } from "../src/core/services";
 
 (async () => {
     let n = 1;
-    const needCNum = await Goods.countDocuments({ itemNameId: { $exists: true } });
+    const needCNum = await Goods.countDocuments({ itemNameId: { $exists: true }, steamMaxBuyPrice: null, steamMinSellPrice: null });
     console.log(needCNum);
     while (true) {
         console.log(`round ${n}`);
-        const allgoods = await Goods.find({ itemNameId: { $exists: true } }).limit(200);
+        const allgoods = await Goods.find({ itemNameId: { $exists: true }, steamMaxBuyPrice: null, steamMinSellPrice: null }).limit(200);
         if (n * 200 > needCNum) {
             console.log(`end!`);
             break;
