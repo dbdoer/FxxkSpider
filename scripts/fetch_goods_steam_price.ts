63add3b44235e19c17e05eb46dce801fd8b18a22
Sammy Liang <sammyliang97@gmail.com>
2019-01-07 10:42:28 +0800

    feat: fetch_goods_steam_price

--- scripts/fetch_goods_steam_price.ts
+++ scripts/fetch_goods_steam_price.ts
@@ -1,10 +1,10 @@
 import { Goods } from "../src/core/model";
-import { getItemNameId } from "../src/core/services";
 import { sleep } from "../src/core/helpers";
+import { getSteamPrice } from "../src/core/services";
 
 (async () => {
     let n = 1;
-    const needCNum = await Goods.countDocuments({ itemNameId: null });
+    const needCNum = await Goods.countDocuments({ itemNameId: { $exists: true } });
     console.log(needCNum);
     while (true) {
         console.log(`round ${n}`);
@@ -15,19 +15,16 @@ import { sleep } from "../src/core/helpers";
         }
         for (const g of allgoods) {
             if (g) {
-                let itemNameId;
                 try {
-                    itemNameId = await getItemNameId(g.gameName, g.marketHashName);
+                    const { steamMaxBuyPrice, steamMinSellPrice } = await getSteamPrice(g.itemNameId);
+                    await (g as any).updateOne({ steamMaxBuyPrice, steamMinSellPrice });
                 } catch (e) {
                     console.log(e);
                     continue;
                 }
-                console.log(itemNameId);
-                await (g as any).updateOne({ itemNameId });
                 await sleep(10000);
             }
         }
         n = n + 1;
     }
-    process.exit(1);
 })();
