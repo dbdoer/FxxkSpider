c6db8b819eea7e3bd7e78b3b75efc909ae118a03
Sammy Liang <sammyliang97@gmail.com>
2019-01-11 11:53:21 +0800

    feat: add Cell for export goods info

--- src/core/apis/task.ts
+++ src/core/apis/task.ts
@@ -4,7 +4,7 @@ import { getGoodsSellingList,
     getGoodsBuyingList,
     parseGoodsBuyingList,
  } from "../services";
-import { Task, Status } from "../model";
+import { Task, Status, Goods } from "../model";
 import xlsx = require("better-xlsx");
 import * as fs from "fs";
 
@@ -102,43 +102,62 @@ export const taskResultExport = async (taskId: string) => {
             const cell3 = row.addCell();
             cell3.value = "steam出售价格（单位：元）";
             const cell4 = row.addCell();
-            cell4.value = "倍数";
+            cell4.value = "steam最大收购价（单位：元）";
             const cell5 = row.addCell();
-            cell5.value = "Buff在售数量";
-            const cell6 = row.addCell();
-            cell6.value = "原始折扣价（百分比）";
+            cell5.value = "steam最小出售价（单位：元）"
+            // const cell6 = row.addCell();
+            // cell6.value = "steam最近24h成交量"
             const cell7 = row.addCell();
-            cell7.value = "原始转回利润（百分比）";
+            cell7.value = "倍数";
             const cell8 = row.addCell();
-            cell8.value = "Buff商品链接";
+            cell8.value = "Buff在售数量";
             const cell9 = row.addCell();
-            cell9.value = "steam商品链接";
+            cell9.value = "原始折扣价（百分比）";
             const cell10 = row.addCell();
-            cell10.value = "商品唯一标识名称";
+            cell10.value = "原始转回利润（百分比）";
+            const cell11 = row.addCell();
+            cell11.value = "Buff商品链接";
+            const cell12 = row.addCell();
+            cell12.value = "steam商品链接";
+            const cell13 = row.addCell();
+            cell13.value = "商品唯一标识名称";
 
             const rawResult = JSON.parse(task.rawResult);
-            rawResult.forEach((r) => {
+            rawResult.forEach(async (r) => {
                 const dataRow = sheet.addRow();
                 const dataCell1 = dataRow.addCell();
                 const dataCell2 = dataRow.addCell();
                 const dataCell3 = dataRow.addCell();
                 const dataCell4 = dataRow.addCell();
                 const dataCell5 = dataRow.addCell();
-                const dataCell6 = dataRow.addCell();
+                // const dataCell6 = dataRow.addCell();
                 const dataCell7 = dataRow.addCell();
                 const dataCell8 = dataRow.addCell();
                 const dataCell9 = dataRow.addCell();
                 const dataCell10 = dataRow.addCell();
+                const dataCell11 = dataRow.addCell();
+                const dataCell12 = dataRow.addCell();
+                const dataCell13 = dataRow.addCell();
                 dataCell1.value = r.name;
                 dataCell2.value = r.sell_min_price;
                 dataCell3.value = r.steam_price_cny;
-                dataCell4.value = r.diff_price;
-                dataCell5.value = r.sell_num;
-                dataCell6.value = r.original_discount_price;
-                dataCell7.value = r.original_profit;
-                dataCell8.value = r.buff_goods_url;
-                dataCell9.value = r.steam_market_url;
-                dataCell10.value = r.market_hash_name;
+                const goods = await Goods.findOne({ marketHashName: r.market_hash_name });
+                if (goods) {
+                    dataCell4.value = goods.steamMaxBuyPrice || "";
+                    dataCell5.value = goods.steamMinSellPrice || "";
+                    // dataCell6.value = goods.volume || "";
+                } else {
+                    dataCell4.value = "";
+                    dataCell5.value = "";
+                    // dataCell6.value = "";
+                }
+                dataCell7.value = r.diff_price;
+                dataCell8.value = r.sell_num;
+                dataCell9.value = r.original_discount_price;
+                dataCell10.value = r.original_profit;
+                dataCell11.value = r.buff_goods_url;
+                dataCell12.value = r.steam_market_url;
+                dataCell13.value = r.market_hash_name;
             });
             break;
         }
